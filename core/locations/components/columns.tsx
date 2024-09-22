9766e3d93a591884f68999238d15746afb26d9d0
Chu <dzhu2409@gmail.com>
2024-09-21 22:52:44 -0500

    Add filter and row actions to locations columns

--- core/locations/components/columns.tsx
+++ core/locations/components/columns.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { DataTableRowActions } from '@/core/shared/components/table/data-table-row-actions'
 import { Location } from '@prisma/client'
 import { ColumnDef } from '@tanstack/react-table'
 
@@ -15,5 +16,11 @@ export const locationColumns: ColumnDef<Location>[] = [
   {
     accessorKey: 'laboratory',
     header: 'Laboratorio',
+    filterFn: (row, id, filterValue) =>
+      filterValue.includes(row.original.laboratory),
+  },
+  {
+    id: 'actions',
+    cell: ({ row }) => <DataTableRowActions row={row} />,
   },
 ]
