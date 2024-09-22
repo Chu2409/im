f43b59009d0bf532f7307024d735dfdc6f53221d
Chu <dzhu2409@gmail.com>
2024-09-21 22:52:00 -0500

    Adding some logic to locations client

--- core/locations/components/client.tsx
+++ core/locations/components/client.tsx
@@ -1,11 +1,43 @@
-import { DataTable } from '@/core/shared/components/data-table'
-import { getLocations } from '../actions/get-locations'
+'use client'
+
+import { Header } from '@/core/shared/components/head/header'
+import { DataTable } from '@/core/shared/components/table/data-table'
+import { Location } from '@prisma/client'
 import { locationColumns } from './columns'
+import { Option } from '@/core/shared/components/table/types'
 
-export const revalidate = 0
+const generateUniqueLaboratoryOptions = (locations: Location[]): Option[] => {
+  const uniqueLaboratories = Array.from(
+    new Set(locations.map((location) => location.laboratory)),
+  )
+  return uniqueLaboratories.map((laboratory) => ({
+    label: laboratory,
+    value: laboratory,
+  }))
+}
 
-export const LocationClient = async () => {
-  const locations = await getLocations()
+export const LocationClient = ({ locations }: { locations: Location[] }) => {
+  const filters = [
+    {
+      key: 'laboratory',
+      values: generateUniqueLaboratoryOptions(locations),
+    },
+  ]
 
-  return <DataTable columns={locationColumns} data={locations} />
+  return (
+    <>
+      <Header
+        title='Locaciones'
+        description='Administra las locaciones o ubicaciones disponibles en los laboratorios.'
+        buttonLabel='Nueva locación'
+      />
+
+      <DataTable
+        data={locations}
+        columns={locationColumns}
+        inputFilterKey='name'
+        filters={filters}
+      />
+    </>
+  )
 }
