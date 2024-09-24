b583bc3cbd474e7e6d5a9d4814e00e1cef8a5063
Chu <dzhu2409@gmail.com>
2024-09-24 12:37:05 -0500

    Add location form

---
+++ core/locations/components/form.tsx
@@ -0,0 +1,164 @@
+'use client'
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/ui/form'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Input } from '@/ui/input'
+import { Button } from '@/ui/button'
+import { Location } from '@prisma/client'
+import { useState } from 'react'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/ui/select'
+import { laboratories } from '../data'
+import { createLocation } from '../actions/create-location'
+import { updateLocation } from '../actions/update-location'
+import { useRouter } from 'next/navigation'
+
+const formSchema = z.object({
+  name: z
+    .string({ message: 'Ingrese el nombre' })
+    .min(5, 'Mínimo 5 caracteres')
+    .max(50, 'Máximo 50 caracteres'),
+  code: z
+    .string({ message: 'Ingrese un código' })
+    .min(2, 'Mínimo 2 caracteres')
+    .max(10, 'Máximo 10 caracteres'),
+  laboratory: z
+    .string({ message: 'Seleccione un laboratorio' })
+    .min(1, 'Seleccione un laboratorio'),
+})
+
+type formType = z.infer<typeof formSchema>
+
+export const LocationForm = ({
+  initialData,
+}: {
+  initialData: Location | null
+}) => {
+  const router = useRouter()
+
+  const form = useForm<formType>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: initialData?.name || '',
+      code: initialData?.code || '',
+      laboratory: initialData?.laboratory || '',
+    },
+  })
+
+  // const toastMessage = initialData ? 'Locación actualizada' : 'Locación creada'
+  const action = initialData ? 'Actualizar locación' : 'Crear locación'
+
+  const [isLoading, setIsLoading] = useState(false)
+
+  const onSubmit = (values: formType) => {
+    try {
+      setIsLoading(true)
+
+      if (initialData) updateLocation(initialData.id, values)
+      else createLocation(values)
+
+      router.push('/locations')
+      router.refresh()
+    } catch (error) {
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
+        <div className='grid gap-6 sm:grid-cols-2 lg:grid-cols-3 w-full mt-4 mb-6'>
+          <FormField
+            control={form.control}
+            name='name'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Nombre</FormLabel>
+
+                <FormControl>
+                  <Input placeholder='Sala 1' disabled={isLoading} {...field} />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name='code'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Código</FormLabel>
+
+                <FormControl>
+                  <Input
+                    placeholder='Tu código de la locación'
+                    disabled={isLoading}
+                    {...field}
+                    value={field.value.toUpperCase()}
+                  />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name='laboratory'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Laboratorio</FormLabel>
+                <FormControl>
+                  <Select
+                    disabled={isLoading}
+                    // eslint-disable-next-line react/jsx-handler-names
+                    onValueChange={field.onChange}
+                    value={field.value}
+                  >
+                    <SelectTrigger>
+                      <SelectValue placeholder='Selecciona un laboratorio' />
+                    </SelectTrigger>
+
+                    <SelectContent>
+                      {laboratories.map((lab) => (
+                        <SelectItem
+                          key={lab.id}
+                          value={lab.laboratory}
+                          className='cursor-pointer'
+                        >
+                          {lab.laboratory}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <Button type='submit'>{action}</Button>
+      </form>
+    </Form>
+  )
+}
