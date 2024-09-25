ad26eb233035c497879ea32bd01e3844192309d2
Chu <dzhu2409@gmail.com>
2024-09-24 22:48:05 -0500

    Update locations seed

--- prisma/data/locations.ts
+++ prisma/data/locations.ts
@@ -8,31 +8,31 @@ export const locations: ILocation[] = [
   {
     name: 'Refrigeradora Piso 1',
     code: 'RP1-L1',
-    laboratory: '1 - Laboratorio Centro',
+    laboratory: 'Centro',
   },
   {
     name: 'Refrigeradora Piso 2',
     code: 'RP2-L1',
-    laboratory: '1 - Laboratorio Centro',
+    laboratory: 'Centro',
   },
   {
     name: 'Sala 1',
     code: 'SA1-L1',
-    laboratory: '1 - Laboratorio Centro',
+    laboratory: 'Centro',
   },
   {
     name: 'Refrigeradora Piso 4',
     code: 'RP4-L2',
-    laboratory: '2 - Laboratorio Sur',
+    laboratory: 'Sur',
   },
   {
     name: 'Refrigeradora Piso 3',
     code: 'RP3-LS',
-    laboratory: '2 - Laboratorio Sur',
+    laboratory: 'Sur',
   },
   {
     name: 'Sala 2',
     code: 'SA2-L2',
-    laboratory: '2 - Laboratorio Sur',
+    laboratory: 'Sur',
   },
 ]
