79a7302f7eced04706b6cd6c176fc8c022cd705a
Chu <dzhu2409@gmail.com>
2024-09-25 20:01:51 -0500

    Add date formatter

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,11 @@
 import { clsx, type ClassValue } from 'clsx'
+import { format } from 'date-fns'
+import { es } from 'date-fns/locale'
+
 import { twMerge } from 'tailwind-merge'
 
 export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs))
+
+export const formatDate = (date: Date) => {
+  return format(date, 'MMMM dd, yyyy', { locale: es })
+}
