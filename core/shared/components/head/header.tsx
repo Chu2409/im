1157aceb07e62eaca264d96053d3c8a45ee5ba35
Chu <dzhu2409@gmail.com>
2024-09-27 09:04:49 -0500

    Change header shared component to be use a button instead of a link

--- core/shared/components/head/header.tsx
+++ core/shared/components/head/header.tsx
@@ -1,37 +1,34 @@
-'use client'
-
 import { Plus } from 'lucide-react'
 import { Separator } from '@/ui/separator'
 import { Heading } from './heading'
-import { usePathname } from 'next/navigation'
-import Link from 'next/link'
+import { Button } from '@/ui/button'
 
 interface HeaderProps {
   title: string
   description: string
   buttonLabel: string
+  onButtonClick: () => void
 }
 
 export const Header: React.FC<HeaderProps> = ({
   title,
   description,
   buttonLabel,
+  onButtonClick,
 }) => {
-  const pathname = usePathname()
-
   return (
     <>
       <div className='flex items-center justify-between gap-4 w-full'>
         <Heading title={title} description={description} />
 
-        <Link
+        <Button
           className='flex justify-center items-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-black hover:opacity-70 cursor-pointer text-center'
-          href={`${pathname}/new`}
+          onClick={onButtonClick}
         >
           <Plus className='mr-2 h-4 w-4' />
 
           {buttonLabel}
-        </Link>
+        </Button>
       </div>
 
       <Separator className='my-1' />
