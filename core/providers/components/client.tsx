4d9f213a8795b36bdc5a2ccf196a3befd90c76da
Chu <dzhu2409@gmail.com>
2024-09-27 09:05:54 -0500

    Add provider modal to client and delete single client

--- core/providers/components/client.tsx
+++ core/providers/components/client.tsx
@@ -1,16 +1,25 @@
+'use client'
+
 import { Header } from '@/core/shared/components/head/header'
 import { DataTable } from '@/core/shared/components/table/data-table'
 import { Provider } from '@prisma/client'
 import { providersColumns } from './columns'
+import { ProvidersModal } from './modal'
+import { useState } from 'react'
 
 export const ProvidersClient = ({ providers }: { providers: Provider[] }) => {
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
     <>
       <Header
         title='Proveedores'
         description='Administra los proveedores de tus productos.'
         buttonLabel='Nuevo proveedor'
+        onButtonClick={() => setIsOpen(true)}
       />
+
+      <ProvidersModal isOpen={isOpen} onClose={() => setIsOpen(false)} />
 
       <DataTable
         data={providers}
