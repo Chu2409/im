2795ab83fc9bce57589b63beb8fb3497f097d2ee
Chu <dzhu2409@gmail.com>
2024-09-30 15:29:27 -0500

    Update product provider seed

--- prisma/data/products-providers.ts
+++ prisma/data/products-providers.ts
@@ -9,8 +9,16 @@ export const productsProviders: IProductProvider[] = [
     providerId: 1,
   },
   {
+    productId: 2,
+    providerId: 2,
+  },
+  {
     productId: 4,
     providerId: 1,
+  },
+  {
+    productId: 4,
+    providerId: 2,
   },
   {
     productId: 5,
