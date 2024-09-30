d8a9ef99e157907b5b7d77d5e1a4bbc395181cdc
Chu <dzhu2409@gmail.com>
2024-09-30 15:03:28 -0500

    Update data table faceted filter

--- core/shared/components/table/data-table-faceted-filter.tsx
+++ core/shared/components/table/data-table-faceted-filter.tsx
@@ -117,11 +117,9 @@ export function DataTableFacetedFilter<TData, TValue>({
 
                     <span>{option.label}</span>
 
-                    {facets?.get(option.value) && (
-                      <span className='ml-auto flex h-4 w-4 items-center justify-center font-mono text-xs'>
-                        {facets.get(option.value)}
-                      </span>
-                    )}
+                    <span className='ml-auto flex h-4 w-4 items-center justify-center font-mono text-xs'>
+                      {facets?.get(option.label) || 0}
+                    </span>
                   </CommandItem>
                 )
               })}
