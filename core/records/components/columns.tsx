1f13b800b084be6b13aee4b34cf6ac558bec6dc8
Chu <dzhu2409@gmail.com>
2024-10-04 20:30:48 -0500

    Add custom data table row actions in records module

--- core/records/components/columns.tsx
+++ core/records/components/columns.tsx
@@ -6,8 +6,8 @@ import { ColumnDef } from '@tanstack/react-table'
 import { useRecord } from '../hooks/use-record'
 import { IRecordWithItems } from '../types'
 import { useRecordModal } from '../hooks/use-record-modal'
-import { DataTableRowActions } from '@/core/shared/components/table/data-table-row-actions'
 import { deleteRecord } from '../actions/delete-record'
+import { DataTableRowActions } from './data-table-row-actions'
 
 export const recordsColumns: ColumnDef<IRecordWithItems>[] = [
   {
@@ -51,8 +51,8 @@ export const recordsColumns: ColumnDef<IRecordWithItems>[] = [
       return (
         <DataTableRowActions
           id={row.original.id}
-          toggleStatus={onDelete}
-          toggleStatusMessage='El registro ha sido eliminado correctamente'
+          onDelete={onDelete}
+          deleteMessage='El registro ha sido eliminado correctamente'
           errorMessage='Elimine los items del registro primero'
           onEdit={handleClick}
         />
