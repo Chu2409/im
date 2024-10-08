a569108d887e6ce69fee1a7d0702f01256081a20
Chu <dzhu2409@gmail.com>
2024-10-07 22:30:26 -0500

    Add lot and location column to items table

--- core/items/components/columns.tsx
+++ core/items/components/columns.tsx
@@ -1,20 +1,39 @@
 'use client'
 
 import { ColumnDef } from '@tanstack/react-table'
-import { IItemWithProduct } from '../types'
+import { IItemWithLotLocation } from '../types'
 import { DataTableColumnHeader } from '@/core/shared/components/table/data-table-column-header'
 
-export const itemsColumns: ColumnDef<IItemWithProduct>[] = [
+export const itemsColumns: ColumnDef<IItemWithLotLocation>[] = [
   {
     accessorKey: 'name',
     header: 'Nombre',
-    cell: ({ row }) => <div className=''>{row.original.product.name}</div>,
+    cell: ({ row }) => (
+      <div className=''>{row.original.lotLocation.lot.product.name}</div>
+    ),
   },
   {
     accessorKey: 'quantity',
     meta: 'Cantidad',
     header: ({ column }) => (
       <DataTableColumnHeader column={column} title='Cantidad' />
+    ),
+  },
+  {
+    accessorKey: 'lot',
+    header: 'Lote',
+    cell: ({ row }) => (
+      <div className=''>{row.original.lotLocation.lot.id}</div>
+    ),
+  },
+  {
+    accessorKey: 'location',
+    header: 'Ubicación',
+    cell: ({ row }) => (
+      <div className=''>
+        {row.original.lotLocation.location.code} -{' '}
+        {row.original.lotLocation.location.laboratory}
+      </div>
     ),
   },
 ]
