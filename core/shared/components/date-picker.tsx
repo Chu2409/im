4611f6baee3108cf0d24e510107209c3a9edb0d5
Chu <dzhu2409@gmail.com>
2024-10-23 11:31:30 -0500

    Update date picker shared component

--- core/shared/components/date-picker.tsx
+++ core/shared/components/date-picker.tsx
@@ -1,36 +1,47 @@
 'use client'
 
-import { useState } from 'react'
-import { format } from 'date-fns'
 import { Calendar as CalendarIcon } from 'lucide-react'
 
-import { cn } from '@/lib/utils'
+import { cn, formatDate } from '@/lib/utils'
 import { Button } from '@/ui/button'
 import { Calendar } from '@/ui/calendar'
 import { Popover, PopoverContent, PopoverTrigger } from '@/ui/popover'
 
-export function DatePicker() {
-  const [date, setDate] = useState<Date>()
+type DatePickerProps = {
+  value: Date
+  onChange: (date?: Date) => void
+  disabled?: boolean
+  className?: string
+}
 
+export function DatePicker({
+  value,
+  onChange,
+  disabled,
+  className,
+}: DatePickerProps) {
   return (
     <Popover>
       <PopoverTrigger asChild>
         <Button
           variant='outline'
           className={cn(
-            'w-[280px] justify-start text-left font-normal',
-            !date && 'text-muted-foreground',
+            'w-full justify-start text-left font-normal capitalize',
+            !value && 'text-muted-foreground',
+            className,
           )}
         >
           <CalendarIcon className='mr-2 h-4 w-4' />
-          {date ? format(date, 'PPP') : <span>Selecciona una fecha</span>}
+          {value ? formatDate(value) : <span>Selecciona una fecha</span>}
         </Button>
       </PopoverTrigger>
-      <PopoverContent className='w-auto p-0'>
+
+      <PopoverContent>
         <Calendar
           mode='single'
-          selected={date}
-          onSelect={setDate}
+          selected={value}
+          onSelect={onChange}
+          disabled={disabled}
           initialFocus
         />
       </PopoverContent>
