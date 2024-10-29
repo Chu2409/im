5b68d0c8a51d3997d0f721706d9cfef85d73b535
Chu <dzhu2409@gmail.com>
2024-10-29 11:18:54 -0500

    Add use notification custom hook

---
+++ core/shared/hooks/use-notification.ts
@@ -0,0 +1,39 @@
+import { useCallback } from 'react'
+import { ActionRes } from '../types'
+import { useRouter } from 'next/navigation'
+
+interface ToastOptions {
+  variant: 'success' | 'destructive'
+  title: string
+  description: string
+}
+
+const useToastNotification = <T>(toast: (options: ToastOptions) => void) => {
+  const router = useRouter()
+
+  const showNotification = useCallback(
+    (toastData: ActionRes<T>, toastTitle: string, toastDescription: string) => {
+      const { data: success, error } = toastData
+
+      if (error) {
+        toast({
+          variant: 'destructive',
+          title: 'Algo salió mal',
+          description: error,
+        })
+      } else if (success) {
+        toast({
+          variant: 'success',
+          title: toastTitle,
+          description: toastDescription,
+        })
+        router.refresh()
+      }
+    },
+    [toast, router],
+  )
+
+  return showNotification
+}
+
+export default useToastNotification
