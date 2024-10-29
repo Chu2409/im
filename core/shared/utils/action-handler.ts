9ebf6b1289a0f0d09aaf190f08cb87a49ead18b7
Chu <dzhu2409@gmail.com>
2024-10-29 11:18:39 -0500

    Update action hanlder

--- core/shared/utils/action-handler.ts
+++ core/shared/utils/action-handler.ts
@@ -3,22 +3,19 @@ import { ActionRes } from '../types'
 
 export const handleAction = async <T>(
   action: () => Promise<T>,
-  path?: string,
+  path: string,
 ): Promise<ActionRes<T>> => {
   try {
     const data = await action()
     return { data }
   } catch (error: unknown) {
     if (typeof error === 'object' && error !== null && 'message' in error) {
-      console.error(
-        path || '[SERVER_ACTION_ERROR]',
-        (error as { message: string }).message,
-      )
+      console.error(path, (error as { message: string }).message)
       return { error: (error as { message: string }).message }
     } else {
-      const newError = new Error('Ha ocurrido un error en el servidor')
-      console.error(path || '[SERVER_ACTION_ERROR]', newError.message)
-      return { error: newError.message }
+      const msg = 'Ha ocurrido un error en el servidor'
+      console.error(path, msg)
+      return { error: msg }
     }
   }
 }
