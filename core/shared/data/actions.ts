e41e2b5e8b05d0b50c0fb2fb3eb95dca826ff187
Chu <dzhu2409@gmail.com>
2024-10-31 14:12:57 -0500

    Add log model and log utility

---
+++ core/shared/data/actions.ts
@@ -0,0 +1,5 @@
+export const ACTIONS = {
+  CREATE: { id: 1, name: 'Creación' },
+  UPDATE: { id: 2, name: 'Actualización' },
+  DELETE: { id: 3, name: 'Eliminación' },
+} as const
--- core/shared/types/index.ts
+++ core/shared/types/index.ts
@@ -1,3 +1,5 @@
+import { ACTIONS } from '../data/actions'
+
 export interface IRoute {
   href: string
   label: string
@@ -18,4 +20,11 @@ export interface IOption<T> {
 export interface ActionRes<T> {
   data?: T
   error?: string
+}
+
+export interface ILog {
+  entityId?: number
+  table: string
+  action: (typeof ACTIONS)[keyof typeof ACTIONS]
+  content: object
 }
