2358732dbb8c1a0379a9581b500e970d677a49cd
Chu <dzhu2409@gmail.com>
2024-11-01 22:55:03 -0500

    Remove error

--- core/providers/actions/create-provider.ts
+++ core/providers/actions/create-provider.ts
@@ -9,12 +9,10 @@ import { Provider } from '@prisma/client'
 interface ICreateProvider extends Omit<Provider, 'id' | 'active'> {}
 
 export const createProvider = async (data: ICreateProvider) => {
-  const createProvider = async () => {
-    throw new Error('Not implemented')
-    return await prisma.provider.create({
+  const createProvider = async () =>
+    await prisma.provider.create({
       data,
     })
-  }
 
   return await handleAction(createProvider, '[CREATE_PROVIDER]', {
     action: ACTIONS.CREATE,
