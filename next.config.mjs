9d76a8f95cef9a568d194039b240a788d165716c
Chu <dzhu2409@gmail.com>
2024-11-02 12:09:43 -0500

    Si

--- next.config.mjs
+++ next.config.mjs
@@ -2,21 +2,21 @@ import { withSentryConfig } from '@sentry/nextjs'
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-const config =
-  process.env.NODE_ENV === 'production'
-    ? withSentryConfig(nextConfig, {
-        org: 'chu-z0',
-        project: 'genomyc',
-        silent: !process.env.CI,
-        reactComponentAnnotation: {
-          enabled: true,
-        },
-        tunnelRoute: '/monitoring',
-        hideSourceMaps: true,
-        disableLogger: true,
-        automaticVercelMonitors: true,
-        widenClientFileUpload: false,
-      })
-    : nextConfig
+const config = withSentryConfig(nextConfig, {
+  org: 'chu-z0',
+  project: 'genomyc',
+  silent: !process.env.CI,
+  reactComponentAnnotation: {
+    enabled: true,
+  },
+  sourcemaps: {
+    disable: true,
+  },
+  tunnelRoute: '/monitoring',
+  hideSourceMaps: true,
+  disableLogger: true,
+  automaticVercelMonitors: true,
+  widenClientFileUpload: false,
+})
 
 export default config
